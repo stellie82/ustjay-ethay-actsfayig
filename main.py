1baa323af6ad43f74aaa60367858bc322c8a552b
stellie <kims82@gmail.com>
2020-12-16 16:49:27 -0800

    create functions to store random fact, then use that fact to input into translator app and post as link, format homepage

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Stella Kim
+# Assignment 5: Ustjay ethay Actsfayig
+
 import os
 
 import requests
@@ -9,20 +12,37 @@ app = Flask(__name__)
 
 def get_fact():
 
-    response = requests.get("http://unkno.com")
+    response = requests.get('http://unkno.com')
 
-    soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    facts = soup.find_all('div', id='content')
 
     return facts[0].getText()
 
 
+def translate(fact):
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    response = requests.post(url, data={'input_text': fact},
+                             allow_redirects=False)
+
+    return response.headers.get('location')
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    body = translate(fact)
+
+    template = f"""
+    <h1>Pig Latin Translator</h1>
+    <p><b>Random Fact</b>: {fact}</p>
+    <p><b>Translation</b>: <a href={body}>{body}</a></p>
+    """
+    # <a href='https://hidden-journey-62459.herokuapp.com/'>Link to Translator</a>
+
+    return Response(response=template, mimetype='text/html')
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6787))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 6787))
     app.run(host='0.0.0.0', port=port)
-
